f4039e786f576001e07507d4ba4a236d0644ba5e
ksingh <ksingh@polariswireless.com>
2023-04-06 12:45:02 +0530

    Initial Commits

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">kehsav singh</header>
+    </div>
+  );
+}
+
+export default App;
